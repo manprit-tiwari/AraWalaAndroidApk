67899e7622f4b326409be73be059c864d9f904e9
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-09-24 21:07:36 +0530

    added route guards for authorized and unauthorized user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+
+const redirectToLogin = () => redirectUnauthorizedTo(['auth/login']);
+const redirectToHome = () => redirectLoggedInTo(['/dashboard']);
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'auth',
-  //   pathMatch: 'full'
-  // },
   {
     path: '',
+    ...canActivate(redirectToLogin),
     loadChildren: () => {
       return import('./layout/layout.module').then((m) => m.LayoutModule);
     }
   },
   {
     path: 'auth',
+    ...canActivate(redirectToHome),
     loadChildren: () => {
       return import('./auth/auth.module').then((m) => m.AuthModule);
     }
