6e46897befe62be8500528cadc02c960389b9548
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-09-24 21:19:39 +0530

    added Profile component and set up routing for it

--- src/app/pages/routing.ts
+++ src/app/pages/routing.ts
@@ -2,9 +2,20 @@ import { Routes } from "@angular/router";
 
 const Routing: Routes = [
     {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
         path: 'dashboard',
         loadChildren: () => {
             return import('./dashboard/dashboard.module').then((m) => m.DashboardModule);
+        }
+    },
+    {
+        path: 'profile',
+        loadChildren: () => {
+            return import('./profile/profile.module').then((m) => m.ProfileModule);
         }
     }
 ];
