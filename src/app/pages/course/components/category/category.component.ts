9ab54210451aaf4ec47cb9ec4c4270e922f60af1
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-10-02 14:47:25 +0530

    style updated in course category page and fixed some styling error in navbar

--- src/app/pages/course/components/category/category.component.ts
+++ src/app/pages/course/components/category/category.component.ts
@@ -1,11 +1,37 @@
 import { Component } from "@angular/core";
 
+export interface courseCategory {
+    name: string,
+    subCategoryNumber: number,
+    thumbnail: string,
+    discription: string
+}
+
 @Component({
     selector: 'course-category',
     templateUrl: './category.component.html',
     styleUrls: ['./category.component.scss']
 })
 export class CourseCategoryComponent {
-    courseCategories: string[] = ['Defence', 'NDA', 'UPSC'];
+    courseCategories: courseCategory[] = [
+        {
+            name: 'Aganiveer',
+            subCategoryNumber: 3,
+            thumbnail: 'assets/images/aganiveer.jpg',
+            discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis dolorum omnis accusamus excepturi vero? Voluptatibus consequatur at tenetur ipsa iste, quidem minus fugiat! Eligendiillo non accusamus eum recusandae!'
+        },
+        {
+            name: 'Defence',
+            subCategoryNumber: 4,
+            thumbnail: 'assets/images/defence.jpg',
+            discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis dolorum omnis accusamus excepturi vero? Voluptatibus consequatur at tenetur ipsa iste, quidem minus fugiat! Eligendiillo non accusamus eum recusandae!'
+        },
+        {
+            name: 'NDA',
+            subCategoryNumber: 2,
+            thumbnail: 'assets/images/nda.jpg',
+            discription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quis dolorum omnis accusamus excepturi vero? Voluptatibus consequatur at tenetur ipsa iste, quidem minus fugiat! Eligendiillo non accusamus eum recusandae!'
+        }
+    ]
     // courseCategories: string[] = [];
 }
